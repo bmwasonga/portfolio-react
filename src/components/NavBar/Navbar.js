41ca06c7fa7c71910e3d2940580f174c76e6218a
bmwasonga <bmwasonga@gmail.com>
2021-06-11 15:04:38 +0300

    changes

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-      <nav className="bg-gray-400">
+      <nav className="bg-indigo-800">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
--- src/components/pages/Skills/Skills.js
+++ src/components/pages/Skills/Skills.js
@@ -1,32 +1,65 @@
 import React from 'react';
-import './styles.css';
 import { front, back } from './skilldata';
+import man from '../../assets/programmer.svg';
 
 function Skills() {
   return (
-    <div className="bg-indigo-900 max-w-full h-screen">
-      <h2>What I do</h2>
+    <div className="bg-indigo-500 max-w-full h-screen">
+      <div className="">
+        <div className="flex content-center justify-center">
+          <h1 className="flex content-center justify-center text-gray-100 xl:text-6xl md:text-6xl sm:text-5xl  mb-8">
+            What I do
+          </h1>
+        </div>
+        <div className="flex bg-gray-400 w-100 ml-10 mr-10 lg:flex-row xl:flex-row md:flex-col sm:flex-col items-center justify-center">
+          <div className="flex items-center justify-center bg-indigo-100 xl:w-4/12 lg:w-4/12 md:w-full sm:w-full xl:h-ful md:h-full m-5">
+            <img
+              className="flex items-center justify-center bg-indigo-900p-10 xl:w-2/3 lg:w-2/3 md:w-2/3 sm:w-full xl:h-4/5 lg:h-4/5 "
+              src={man}
+              alt="man sitting on a table"
+            />
+          </div>
+          <div className="bg-red-700 m-5 xl:w-4/6">
+            <h2 className="text-gray-100 lg:text5xl md:text-4xl sm:text-3xl p-2 text-center">
+              I am currently working and exploring with the following:
+            </h2>
 
-      <p>I am currently learning and exploring the following:</p>
-      <h2>Here's what I know:</h2>
-      <ul className="pack-icons">
-        {front.map((packIcon) => {
-          const { id, icon } = packIcon;
-          return <li key={id}>{icon}</li>;
-        })}
-      </ul>
+            <h3 className="text-gray-100 lg:text5xl md:text-4xl sm:text-3xl p-2 text-center">
+              Front-end:
+            </h3>
 
-      <h2>Here's what I am learning:</h2>
-      <ul className="pack-icons">
-        {back.map((packIcon) => {
-          const { id, icon } = packIcon;
-          return <li key={id}>{icon}</li>;
-        })}
-      </ul>
-      <h2>What do i want to do next? </h2>
-      <p>
-        I aspire to be a fullstack Developer and I am working on that everyday.
-      </p>
+            <ul className="flex justify-center content-center">
+              {front.map((packIcon) => {
+                const { id, icon } = packIcon;
+                return (
+                  <li
+                    className="text-gray-400 hover:text-gray-700 text-5xl p-2"
+                    key={id}
+                  >
+                    {icon}
+                  </li>
+                );
+              })}
+            </ul>
+            <h3 className="text-gray-100 lg:text5xl md:text-4xl sm:text-3xl p-2 text-center">
+              Back-end:
+            </h3>
+            <ul className="flex justify-center content-center">
+              {back.map((packIcon) => {
+                const { id, icon } = packIcon;
+                return (
+                  <li
+                    className="text-gray-400 hover:text-gray-700 text-5xl p-2"
+                    key={id}
+                  >
+                    {icon}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
