91e8cd1df85d9d940547bdddecdfc672cad582de
bmwasonga <bmwasonga@gmail.com>
2021-08-28 04:08:05 +0300

    new stuff

--- README.md
+++ README.md
@@ -1,14 +1,1 @@
-### `About me`
-
-My name is Ben Wasonga.
-I am a trained teacher and a self-taught developer from Kenya.
-
-### `React`
-
-I have been learning React and is by far the best web frameworks that i have worked with
-
-### `Experience`
-
-I would not yet consider myself an expert, as I continue to learn, I challenge myself to do more and better.
-To create and to connect.
-To be considered a somebody in this feild
+**_Ben Wasonga_**
--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import Navbar from './components/NavBar/Navbar';
-import Home from './components/pages/Home/Home';
-import Skills from './components/pages/Skills/Skills';
-import Projects from './components/pages/Projects/Projects';
-import Footer from './components/pages/Footer/Footer';
+import React from "react";
+import "./App.scss";
+import Main from "./containers/Main";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
-      <Skills />
-      <Projects />
-      <Footer />
+    <div>
+      <Main />
     </div>
   );
 }
