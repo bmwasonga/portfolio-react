ab6e485256a8342a6923e5ae2a36bee49d76c4c9
bmwasonga <bmwasonga@gmail.com>
2021-08-29 14:09:46 +0300

    .

--- src/portfolio.js
+++ src/portfolio.js
@@ -206,7 +206,7 @@ const bigProjects = {
       ],
     },
   ],
-  display: true, // Set false to hide this section, defaults to true
+  display: false, // Set false to hide this section, defaults to true
 };
 
 // Achievement Section
@@ -264,7 +264,7 @@ const achievementSection = {
       ],
     },
   ],
-  display: true, // Set false to hide this section, defaults to true
+  display: false, // Set false to hide this section, defaults to true
 };
 
 // Blogs Section
